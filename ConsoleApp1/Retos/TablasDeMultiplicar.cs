b8da366ae60e0d1ea4d1949ec24ab15d706a2ba7
Imanol Solano <imanolsolano13@gmial.com>
2026-06-11 20:08:34 -0600

    Implement multiplication table

---
+++ ConsoleApp1/Retos/TablasDeMultiplicar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Retos
+{
+    internal class TablasDeMultiplicar
+    {
+        public static void Ejecutar() 
+        {
+            Console.WriteLine("Tablas de multiplicar del 1 al 10");
+            Console.Write("Ingresa el número entero a multiplicar: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
