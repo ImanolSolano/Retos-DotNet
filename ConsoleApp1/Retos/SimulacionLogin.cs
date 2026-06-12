f8b1a6334d670a8b695e9baa690b39c860e564b5
Imanol Solano <imanolsolano13@gmial.com>
2026-06-11 20:12:47 -0600

    Create login simulation

---
+++ ConsoleApp1/Retos/SimulacionLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Retos
+{
+    internal class SimulacionLogin
+    {
+        public static void Ejecutar() 
+        {
+            Console.WriteLine("Bienvenido");
+
+            Console.Write("Usuario: ");
+            string usuario = Console.ReadLine();
+
+            Console.Write("Contraseña: ");
+            string contraseña = Console.ReadLine();
+
+            if (usuario == "admin" && contraseña == "1234")
+            {
+                Console.WriteLine("¡Acceso concedido!");
+            }
+            else
+            {
+                Console.WriteLine("¡Acceso denegado!");
+                Console.WriteLine("Usuario o contraseña incorrecto");
+            }
+        }
+    }
+}
