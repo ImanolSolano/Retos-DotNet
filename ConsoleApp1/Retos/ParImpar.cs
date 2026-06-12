0825653d9704d143b98a630f6d7dda9676ea11a0
Imanol Solano <imanolsolano13@gmial.com>
2026-06-11 20:03:10 -0600

    Add even or odd validation

---
+++ ConsoleApp1/Retos/ParImpar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Retos
+{
+    internal class ParImpar
+    {
+        public static void Ejecutar() 
+        {
+            Console.WriteLine("Hola, averiguemos si es par o impar");
+            Console.Write("Ingresa el número entero a evaluar: ");
+            int parImpar = Convert.ToInt32(Console.ReadLine());
+
+            if (parImpar % 2 == 0)
+            {
+                Console.WriteLine("El numero ingresado es par");
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado es impar");
+            }
+        }
+    }
+}
